859fb27810981818a2f51018ea6ac8cdfd35ea66
hiresfilip <46921093+hiresfilip@users.noreply.github.com>
2022-06-24 22:05:26 +0200

    02 - Věnování, CSS, JS, úpravy

--- js/version.js
+++ js/version.js
@@ -1,1 +1,3 @@
-console.log('Verze 1.0 - Akordeonový orchestr Andante - update: 24.6.2022 18:07');
+console.log('Verze 1.1 - Akordeonový orchestr Andante - update: 24.6.2022 22.00');
+
+/* Verze 1.0 - Akordeonový orchestr Andante - update: 24.6.2022 18:07 */
